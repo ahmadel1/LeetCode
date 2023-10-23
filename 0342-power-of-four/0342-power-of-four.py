ef6c8d40d79b958af976d0cccfee75b57c5ec632
Ahmed Adel <73443649+ahmadel1@users.noreply.github.com>
2023-10-23 12:07:37 +0300

    Time: 52 ms (6.14%), Space: 24.4 MB (12.41%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -5,6 +5,5 @@ class Solution(object):
             return False
         if n == 1:
             return True
-        x =  int(ln(n) / ln(4))
-        return pow(4, int(x)) == n
+        return pow(4, int(ln(n) / ln(4))) == n
         
